c81115f70848d403a0ad8f5cd30123d3c868f209
Elod Tobak <tobakelod@gmail.com>
2023-12-29 13:11:09 +0100

    Add links to dashboard, reservation, and messages in footer

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -1,3 +1,4 @@
+import { Link } from "react-router-dom";
 import { useAuth } from "../protectedRoute";
 
 const Footer = () => {
@@ -8,8 +9,8 @@ const Footer = () => {
     <footer className="navbar bg-base-primary">
       {loggedIn && (
         <div className="btm-nav">
-          <a
-            href="/dashboard"
+          <Link
+            to="/dashboard"
             className={active == "dashboard" ? "active" : ""}
           >
             <svg
@@ -21,9 +22,9 @@ const Footer = () => {
             >
               <path d="M575.8 255.5c0 18-15 32.1-32 32.1h-32l.7 160.2c0 2.7-.2 5.4-.5 8.1V472c0 22.1-17.9 40-40 40H456c-1.1 0-2.2 0-3.3-.1c-1.4 .1-2.8 .1-4.2 .1H416 392c-22.1 0-40-17.9-40-40V448 384c0-17.7-14.3-32-32-32H256c-17.7 0-32 14.3-32 32v64 24c0 22.1-17.9 40-40 40H160 128.1c-1.5 0-3-.1-4.5-.2c-1.2 .1-2.4 .2-3.6 .2H104c-22.1 0-40-17.9-40-40V360c0-.9 0-1.9 .1-2.8V287.6H32c-18 0-32-14-32-32.1c0-9 3-17 10-24L266.4 8c7-7 15-8 22-8s15 2 21 7L564.8 231.5c8 7 12 15 11 24z" />
             </svg>
-          </a>
-          <a
-            href="/reservation"
+          </Link>
+          <Link
+            to="/reservation"
             className={active == "reservation" ? "active" : ""}
           >
             <svg
@@ -35,8 +36,8 @@ const Footer = () => {
             >
               <path d="M256 80c0-17.7-14.3-32-32-32s-32 14.3-32 32V224H48c-17.7 0-32 14.3-32 32s14.3 32 32 32H192V432c0 17.7 14.3 32 32 32s32-14.3 32-32V288H400c17.7 0 32-14.3 32-32s-14.3-32-32-32H256V80z" />
             </svg>
-          </a>
-          <a href="/messages" className={active == "messages" ? "active" : ""}>
+          </Link>
+          <Link to="/messages" className={active == "messages" ? "active" : ""}>
             <svg
               xmlns="http://www.w3.org/2000/svg"
               height="16"
@@ -46,7 +47,7 @@ const Footer = () => {
             >
               <path d="M64 0C28.7 0 0 28.7 0 64V352c0 35.3 28.7 64 64 64h96v80c0 6.1 3.4 11.6 8.8 14.3s11.9 2.1 16.8-1.5L309.3 416H448c35.3 0 64-28.7 64-64V64c0-35.3-28.7-64-64-64H64z" />
             </svg>
-          </a>
+          </Link>
         </div>
       )}
     </footer>
