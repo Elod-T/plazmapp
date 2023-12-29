51329ac89882067e3106494d9a03a1fb8d08fa0f
Elod Tobak <tobakelod@gmail.com>
2023-12-29 10:30:11 +0100

    Update BasePage width to 300px

--- src/components/BasePage.tsx
+++ src/components/BasePage.tsx
@@ -5,7 +5,7 @@ const BasePage = ({ children }: { children: React.ReactNode }) => {
   return (
     <>
       <Header />
-      <main className="mx-auto w-[400px] xs:w-[450px] sm:w-[620px] md:w-[760px] mg:w-[800px] lg:w-[1100px] xl:w-[1100px] 2xl:w-[1280px]">
+      <main className="mx-auto w-[300px] xs:w-[450px] sm:w-[620px] md:w-[760px] mg:w-[800px] lg:w-[1100px] xl:w-[1100px] 2xl:w-[1280px]">
         <div className="mx-auto w-fit">{children}</div>
       </main>
       <Footer />
