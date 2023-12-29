1fa1f8a976b360439d58b7c6b3599aea188f7e66
Elod Tobak <tobakelod@gmail.com>
2023-12-29 10:30:38 +0100

    Add caching for open timeslots

--- src/pages/NewReservation.tsx
+++ src/pages/NewReservation.tsx
@@ -3,10 +3,25 @@ import { Button } from "../components/design";
 import { PlazmAPPContext } from "../App";
 import { Day } from "../types";
 import BasePage from "../components/BasePage";
+import type PlazmAPP from "../api";
 
 interface TimeSlots {
   [key: string]: string[];
 }
+
+export const getTimeslots = async (plazmAPP: PlazmAPP) => {
+  const openDays = await plazmAPP.getOpenDays();
+
+  const newTimeSlots: TimeSlots = {};
+
+  await Promise.all(
+    openDays.opendays.map(async (day) => {
+      const timeSlots = await plazmAPP.getTimeslots(day.day);
+      newTimeSlots[day.day] = timeSlots;
+    })
+  );
+  return { newTimeSlots, openDays };
+};
 
 const NewReservation = () => {
   const context = useContext(PlazmAPPContext);
@@ -16,24 +31,42 @@ const NewReservation = () => {
   const [selectedTime, setSelectedTime] = useState<string>("");
 
   useEffect(() => {
-    const getOpenDays = async () => {
-      const openDays = await plazmAPP.getOpenDays();
+    const getOpenTimes = async () => {
+      const cachedOpenTimes = localStorage.getItem("openTimes");
+
+      if (cachedOpenTimes) {
+        const { openDays, timeSlots, createdAt } = JSON.parse(
+          cachedOpenTimes
+        ) as {
+          openDays: Day[];
+          timeSlots: TimeSlots;
+          createdAt: number;
+        };
+
+        if (new Date().getTime() - createdAt < 1000 * 60 * 5) {
+          setTimeSlots(timeSlots);
+          setOpenDays(openDays);
+          setLoading(false);
+          return;
+        }
+      }
+
+      const { newTimeSlots, openDays } = await getTimeslots(plazmAPP);
+      setTimeSlots(newTimeSlots);
       setOpenDays(openDays.opendays);
 
-      const newTimeSlots: TimeSlots = {};
+      const data = {
+        openDays: openDays.opendays,
+        timeSlots: newTimeSlots,
+        createdAt: new Date().getTime(),
+      };
 
-      await Promise.all(
-        openDays.opendays.map(async (day) => {
-          const timeSlots = await plazmAPP.getTimeslots(day.day);
-          newTimeSlots[day.day] = timeSlots;
-        })
-      );
-      setTimeSlots(newTimeSlots);
+      localStorage.setItem("openTimes", JSON.stringify(data));
 
       setLoading(false);
     };
 
-    getOpenDays();
+    getOpenTimes();
   }, []);
 
   if (!context) {
@@ -65,16 +98,19 @@ const NewReservation = () => {
       <div className="mx-auto w-full my-10">
         {loading ? (
           <div className="flex flex-col gap-y-3">
-            <div className="skeleton w-96 h-52 mx-auto"></div>
-            <div className="skeleton w-96 h-52 mx-auto"></div>
-            <div className="skeleton w-96 h-52 mx-auto"></div>
+            <div className="skeleton w-full max-w-96 h-52 mx-auto"></div>
+            <div className="skeleton w-full max-w-96 h-52 mx-auto"></div>
+            <div className="skeleton w-full max-w-96 h-52 mx-auto"></div>
           </div>
         ) : (
           <div>
             {openDays.length > 0 ? (
-              <div className="mx-auto w-sm flex flex-col gap-y-5">
+              <div className="mx-auto flex flex-col gap-y-5">
                 {openDays.map((day) => (
-                  <div key={day.day} className="mx-auto card w-96 bg-base-200">
+                  <div
+                    key={day.day}
+                    className="mx-auto w-full max-w-96 card bg-base-200"
+                  >
                     <div className="card-body">
                       <div>
                         {new Date(day.day).toLocaleDateString("hu")}{" "}
@@ -82,11 +118,11 @@ const NewReservation = () => {
                       </div>
 
                       <select
-                        className="select select-bordered select-sm w-full max-w-xs"
-                        defaultValue="Válassz időpontot"
+                        className="select select-bordered select-sm"
                         value={selectedTime}
                         onChange={(e) => setSelectedTime(e.target.value)}
                       >
+                        <option selected>Válassz időpontot</option>
                         {timeSlots[day.day].map((time) => (
                           <option key={time} value={time}>
                             {new Date(time).toLocaleTimeString("hu", {
